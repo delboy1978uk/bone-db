b54041b644ca2314f0d8dace395bf6d73499111c
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-02-27 00:15:53 +0100

    add PDo connection to container if exists

--- src/DbPackage.php
+++ src/DbPackage.php
@@ -6,6 +6,8 @@ namespace Bone\Db;
 
 use Barnacle\Container;
 use Barnacle\RegistrationInterface;
+use Bone\Exception;
+use PDO;
 
 class DbPackage implements RegistrationInterface
 {
@@ -14,6 +16,25 @@ class DbPackage implements RegistrationInterface
      */
     public function addToContainer(Container $c)
     {
+        if ($c->has('db')) {
+            $c[PDO::class] = $c->factory(function (Container $c): PDO {
+                $credentials = $c->get('db');
+                $host = $credentials['host'];
+                $db = $credentials['database'];
+                $user = $credentials['user'];
+                $pass = $credentials['pass'];
+
+                $dbConnection = new PDO('mysql:host=' . $host . ';dbname=' . $db, $user, $pass, [
+                    PDO::ATTR_EMULATE_PREPARES => false,
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                ]);
+
+                return $dbConnection;
+            });
+        } else {
+            throw new Exception('I18nPackage is registered but there is no db config. See the 
+            delboy1978uk/bone-db README.', 418);
+        }
     }
 
     /**
